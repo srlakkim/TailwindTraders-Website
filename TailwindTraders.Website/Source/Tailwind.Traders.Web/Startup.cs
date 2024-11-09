bf558007882eab44deae1768aae23c9eadb19e4f
srlakkim <srlakkim@gmail.com>
2024-11-09 07:14:45 -0800

    Update Startup.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-
+// test PR trigger 
 namespace Tailwind.Traders.Web
 {
     public class Startup
